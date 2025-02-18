da8d7a9896660bd187d1a6beb47c9ec789350a6b
Arvind Kharwal <karvind629@gmail.com>
2025-02-18 12:43:27 +0530

    18 February 2025

--- Django/urls.py
+++ Django/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home,name='home'),
+    path('myfun/', views.myfun,name='myfun'),
+    path('home/', views.home,name='home'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse('Home')
 # Create your views here.
+def myfun(request):
+    return HttpResponse("Hello, Arvind!")
