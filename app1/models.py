9d6a67777dc702df015bd86031ecd43c35f18a7a
Arvind Kharwal <karvind629@gmail.com>
2025-03-05 15:52:03 +0530

    05 March 2025

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
 # Create your models here.
