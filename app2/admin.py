43da2b9750665da9fce3ad6bd884cd141376cf14
Arvind Kharwal <karvind629@gmail.com>
2025-03-06 14:22:44 +0530

    06 March 2025

--- app2/admin.py
+++ app2/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from app2.models import Profile
 # Register your models here.
-admin.site.register(Profile)
+#admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone']
+
+admin.site.register(Profile, ProfileAdmin)
