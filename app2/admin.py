97f6e678b1cf6f6f1cea2d69ffa645c2da8a0c94
Arvind Kharwal <karvind629@gmail.com>
2025-03-06 14:29:30 +0530

    06 March 2025

--- app2/admin.py
+++ app2/admin.py
@@ -3,7 +3,14 @@ from app2.models import Profile
 # Register your models here.
 #admin.site.register(Profile)
 
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['name', 'email', 'phone']
+# class ProfileAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'email', 'phone', 'age']
 
-admin.site.register(Profile, ProfileAdmin)
+# admin.site.register(Profile, ProfileAdmin)
+
+
+# using decorator
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone', 'age']
